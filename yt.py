40888a8132039bb195493843e1700bb860527d81
Miguel Rico <49460746+miguelric@users.noreply.github.com>
2022-05-22 13:04:03 -0500

    Update yt.py

--- yt.py
+++ yt.py
@@ -1,46 +1,31 @@
-# importing packages
-
 from turtle import update
 from pytube import YouTube
 import os
-
 from pytube import Playlist
 
 for file in os.listdir(os.getcwd()):
     if file.endswith(".txt"):
-    # update file exists
-        updatefile = file
-
+        updatefile = file   # update file exists
     else:
         # update file does not exist so make one
-
         f = open("updateFile.txt", "w")
         f.close()
        
-
 # check update file if exists
 # check if new urls are found
 
-
 def getVid(link):
-
-        
     # url input from user
     yt = link
-
-
+    
     # extract only audio
     video = yt.streams.filter(only_audio=True).first()
     
     # check for destination to save file
-
     # print("Enter the destination (leave blank for current directory)")
-
     # destination = str(input(">> ")) or '.'
 
-    destination =  '.'
-  
-    
+    destination =  '.'    
     # download the file
     out_file = video.download(output_path=destination)
     
@@ -53,15 +38,11 @@ def getVid(link):
     print(yt.title + " has been successfully downloaded.")
 
 def getPlay(plList):
-
     for link in plList:
-
-        
 
         # url input from user
         yt = YouTube(link)
-
-
+        
         # extract only audio
         video = yt.streams.filter(only_audio=True).first()
         
@@ -86,28 +67,19 @@ def getPlayNames(playlists):
     urls = []
  
     for playlist in playlists:
-
         purl = Playlist(playlist)
 
         for url in purl:
             urls.append(url)
     return(urls)
 
-
 def updateList():
     f = open(updatefile, "w")
-
     for x in names:
         f.write("\n")
         f.write(x)
     f.close()
-
     print(updatefile)
-
-
-
-
-
 
 prompt = str(input("Would you like a playlist or video: \n>> "))
 pls = []
@@ -117,22 +89,13 @@ if prompt.upper()== "PLAYLIST":
     pls.append(pl)
     # get list of all playlist URLs
 
-
-
-
     names = getPlayNames(pls)
 
-    
     getPlay(getPlayNames(pls))
-
     updateList()
 
 elif prompt.upper()== "VIDEO":
     vid = YouTube(str(input("Enter the URL of the video you want to download: \n>> ")))
     getVid(vid)
-  
 else:
     print("INVALID INPUT")
-
-
-
